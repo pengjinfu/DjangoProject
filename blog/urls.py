679d1c616540de3a6dfce18e7d06adec1b8a1f14
pengjinfu <pengjinfupy@163.com>
2018-09-18 19:41:53 +0800

    绑定 URL 与视图函数,编写视图函数,配置项目 URL

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# Author:Dreamer
+# Time:2018.9.1
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+]
